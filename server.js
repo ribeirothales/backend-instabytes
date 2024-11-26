b21502af12b3d7623e931df40e9c87c1008fbb04
ribeirothales <thales.o.ribeiro@gmail.com>
2024-11-25 22:18:36 -0300

    deploy do projeto

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import routes from './src/routes/postRoutes.js';
+
+const app = express();
+app.use(express.static("uploads"))
+routes(app);
+
+app.listen(3000, () => {
+    console.log('Servidor escutando...');
+});
+
+
+
+
+
+// function buscarPostPorID(id) {
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id)
+//     })
+// }
+
+// app.get('/posts/:id', (req, res) => {
+//     const index = buscarPostPorID(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
